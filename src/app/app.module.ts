edc7484ed93e8c02804d9d1bd9b78296e8b76c35
Balogh Csaba <35346323+Csabesz1130@users.noreply.github.com>
2024-04-29 22:16:13 +0200

    Update AppModule to include services and components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppVideoUploadComponent } from './components/app-video-upload/app-video-upload.component';
+import { AppTranscriptDisplayComponent } from './components/app-transcript-display/app-transcript-display.component';
+import { AppChapterCustomizationComponent } from './components/app-chapter-customization/app-chapter-customization.component';
+
+import { VideoUploadService } from './services/video-upload.service';
+import { TranscriptDisplayService } from './services/transcript-display.service';
+import { ChapterCustomizationService } from './services/chapter-customization.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppVideoUploadComponent,
+    AppTranscriptDisplayComponent,
+    AppChapterCustomizationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    VideoUploadService,
+    TranscriptDisplayService,
+    ChapterCustomizationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
